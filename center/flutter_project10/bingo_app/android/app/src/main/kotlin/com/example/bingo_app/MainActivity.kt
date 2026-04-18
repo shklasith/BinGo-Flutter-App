2eae1908e5136caa4093a8dfe0c0ade4df5cb2eb
Kumudu-Sameeksha <dkkskulasuriya@students.nsbm.ac.lk>
2026-04-19 00:00:40 +0530

    Delete center/flutter_project10/bingo_app directory

--- center/flutter_project10/bingo_app/android/app/src/main/kotlin/com/example/bingo_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bingo_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- center/flutter_project10/bingo_app/lib/ui/shared/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static const Color primary = Color(0xFF0EA5E9);
-  static const Color muted = Color(0xFF6B7280);
-}
